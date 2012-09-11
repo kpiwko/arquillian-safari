8eb96008b2fc2b8c5e1f6b1a41c95086c9827bb9
Karel Piwko <kpiwko@redhat.com>
2012-09-11 16:37:33 +0200

    Package application: Package a WAR

---
+++ src/test/java/com/acme/example/test/Deployments.java
@@ -0,0 +1,28 @@
+package com.acme.example.test;
+
+import java.io.File;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import com.acme.example.data.MemberRepository;
+import com.acme.example.model.Member;
+import com.acme.example.rest.MemberService;
+import com.acme.example.service.MemberRegistration;
+import com.acme.example.util.Resources;
+
+public class Deployments {
+
+    public static WebArchive createDeployment() {
+        WebArchive war = ShrinkWrap
+                .create(WebArchive.class)
+                // add classes
+                .addPackages(true, Member.class.getPackage(), MemberService.class.getPackage(), Resources.class.getPackage(),
+                        MemberRepository.class.getPackage(), MemberRegistration.class.getPackage())
+                // add configuration - resources from src/main/resources
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml").addAsWebInfResource("arquillian-ds.xml");
+
+        // add web pages and other resources
+        return ArchiveUtils.addWebResourcesRecursively(war, new File("src/main/webapp"));
+    }
+}
