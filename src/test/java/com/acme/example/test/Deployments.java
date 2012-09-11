a9b00441c0781a0a02358f2828e5d7b433a2f4bd
Karel Piwko <kpiwko@redhat.com>
2012-09-11 17:22:32 +0200

    Package application: Add ShrinkWrap Resolver

--- src/test/java/com/acme/example/test/Deployments.java
+++ src/test/java/com/acme/example/test/Deployments.java
@@ -4,6 +4,9 @@ import java.io.File;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+import org.jboss.shrinkwrap.resolver.api.maven.filter.ScopeFilter;
 
 import com.acme.example.data.MemberRepository;
 import com.acme.example.model.Member;
@@ -23,6 +26,11 @@ public class Deployments {
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml").addAsWebInfResource("arquillian-ds.xml");
 
         // add web pages and other resources
-        return ArchiveUtils.addWebResourcesRecursively(war, new File("src/main/webapp"));
+        war = ArchiveUtils.addWebResourcesRecursively(war, new File("src/main/webapp"));
+
+        // add required libraries
+        File[] requiredLibraries = DependencyResolvers.use(MavenDependencyResolver.class).loadEffectivePom("pom.xml").importAnyDependencies(new ScopeFilter("", "compile")).resolveAsFiles();
+
+        return war.addAsLibraries(requiredLibraries);
     }
 }
