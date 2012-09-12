81e0ee15f4f19e985f2ba78406dd902dc470cae3
Karel Piwko <kpiwko@redhat.com>
2012-09-12 18:52:23 +0200

    Mobiles: Tailoring test case

--- src/test/java/com/acme/example/test/DroneTest.java
+++ src/test/java/com/acme/example/test/DroneTest.java
@@ -1,5 +1,8 @@
 package com.acme.example.test;
 
+import static org.jboss.arquillian.graphene.Graphene.element;
+import static org.jboss.arquillian.graphene.Graphene.waitModel;
+
 import java.net.URL;
 
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -11,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.android.AndroidDriver;
 
 @RunWith(Arquillian.class)
 public class DroneTest {
@@ -35,10 +39,23 @@ public class DroneTest {
     public void addUser() throws Exception {
         driver.get(contextPath.toString());
 
+        if (driver instanceof AndroidDriver) {
+            waitModel().withMessage("Add button is not present.")
+                    .until(element(By.id("addMember")).isVisible());
+
+            driver.findElement(By.id("addMember")).click();
+        }
+
+        waitModel().withMessage("Registration screen is not present.")
+                .until(element(By.id("name")).isVisible());
+
         driver.findElement(By.id("name")).sendKeys("Samuel");
         driver.findElement(By.id("email")).sendKeys("samuel@vimes.dw");
         driver.findElement(By.id("phoneNumber")).sendKeys("1234567890");
         driver.findElement(By.id("register")).submit();
+
+        waitModel().withMessage("Registration screen did not occur within 10 seconds.")
+                .until(element(By.cssSelector("span.success")).isVisible());
 
     }
 }
